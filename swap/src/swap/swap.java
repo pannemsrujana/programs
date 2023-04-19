ddf2a038b08d75f1e2a07f25b3f3061932aa2a75
pannemsrujana <97735982+pannemsrujana@users.noreply.github.com>
2023-04-19 13:32:23 +0530

    Add files via upload

---
+++ swap/src/swap/swap.java
@@ -0,0 +1,19 @@
+package swap;
+
+public class swap {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a[]= {2,6,1,4,9};
+		int c=4;
+		int b=5;
+		int temp;
+		temp=c;
+		c=b;
+		b=temp;
+		System.out.println(c);
+		System.out.println(b);
+	}
+
+}
