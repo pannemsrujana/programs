3922e39ef35c5fbd39edb0a65f32a3a348014d35
pannemsrujana <97735982+pannemsrujana@users.noreply.github.com>
2023-04-19 13:32:51 +0530

    Add files via upload

---
+++ forloop (2).java
@@ -0,0 +1,16 @@
+package javaloops;
+
+public class forloop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      for(int i=5;i>=1;i--)
+      {
+    	  if(i==3)
+    		   System.out.println("3 is displayed");
+    	  else
+    		  System.out.println("i didnot fined");
+      }
+	}
+
+}
