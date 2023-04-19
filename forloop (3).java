c0b41841f6a14499827b261cab80021eb64263c7
pannemsrujana <97735982+pannemsrujana@users.noreply.github.com>
2023-04-19 13:33:23 +0530

    Add files via upload

---
+++ forloop (3).java
@@ -0,0 +1,14 @@
+package javaloops;
+
+public class forloop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      int i=0;
+      while(i<5)
+      {
+    	  System.out.println(i);
+      }
+	}
+
+}
