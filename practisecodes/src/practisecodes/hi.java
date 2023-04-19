2cf5eb687a6acd4324c6f0f486550284b94ca8a8
pannemsrujana <97735982+pannemsrujana@users.noreply.github.com>
2023-04-19 13:29:27 +0530

    Add files via upload

---
+++ practisecodes/src/practisecodes/hi.java
@@ -0,0 +1,12 @@
+package practisecodes;
+
+public class hi {
+     static int a=5;
+	public static void main(String[] args) {
+		System.out.println(a);
+		System.out.println("hello");
+		System.out.println("hello world");
+
+	}
+
+}
