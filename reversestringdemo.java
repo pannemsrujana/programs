acd5caad946f7365817d64b0034aa6fa916307fa
pannemsrujana <97735982+pannemsrujana@users.noreply.github.com>
2023-04-19 13:34:45 +0530

    Add files via upload

---
+++ reversestringdemo.java
@@ -0,0 +1,18 @@
+package reversestring;
+
+public class reversestringdemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    String s="amma";
+    String t="";
+    for(int i=s.length()-1;i>=0;i--)
+    {
+     
+    	t=t+s.charAt(i);
+    
+    }
+    System.out.println(t);
+	}
+
+}
