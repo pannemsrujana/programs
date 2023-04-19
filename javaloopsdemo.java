71ebc54832f725a937c690ed7841d04d4f5e965f
pannemsrujana <97735982+pannemsrujana@users.noreply.github.com>
2023-04-19 13:34:29 +0530

    Add files via upload

---
+++ javaloopsdemo.java
@@ -0,0 +1,17 @@
+package javaloops;
+
+public class javaloopsdemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       if(5<2)
+       {
+    	   System.out.println("success");
+       }
+       else
+       {
+    	   System.out.println("fail");
+       }
+	}
+
+}
